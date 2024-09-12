753b926af02de3c45c2222ecfcea9b98e5a2ecbb
Wallace Vilela <wallace.vilela9@gmail.com>
2024-09-12 11:58:40 -0300

    feat: initial config

---
+++ src/main/java/wvsdev/backendpicpayprojeto/BackendPicpayApplication.java
@@ -0,0 +1,13 @@
+package wvsdev.backendpicpayprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPicpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendPicpayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wvsdev/backendpicpayprojeto/BackendPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package wvsdev.backendpicpayprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPicpayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
