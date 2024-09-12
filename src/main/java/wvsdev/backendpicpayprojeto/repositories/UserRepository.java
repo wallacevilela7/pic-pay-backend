d4cc06e68ce42023885d14cbf4911e455d593ed4
Wallace Vilela <wallace.vilela9@gmail.com>
2024-09-12 11:59:38 -0300

    feat: create UserRepository

---
+++ src/main/java/wvsdev/backendpicpayprojeto/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package wvsdev.backendpicpayprojeto.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import wvsdev.backendpicpayprojeto.domain.user.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+}
